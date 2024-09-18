b979c5200e4672ba8d0c8dd11d60d68ff85116d6
yakup akyürek <akyurek3466@icloud.com>
2024-09-18 19:36:50 +0300

    lesson1

---
+++ Project1Portfolio/Controllers/EducationController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project1Portfolio.Controllers
+{
+    public class EducationController : Controller
+    {
+        // GET: Education
+        public ActionResult EducationList()
+        {
+            return View();
+        }
+        public ActionResult CreateEducaiton() 
+        {
+            return View();
+        }
+    }
+}
